ecf6379b951e857df80ec02a920a20a4be59151e
Scott White <whitews@gmail.com>
2014-02-19 11:04:47 -0500

    bump version to 0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='FlowIO',
-    version='0.1',
+    version='0.2',
     packages=['flowio'],
     package_data={'': []},
     description='Flow Cytometry Standard I/O',
