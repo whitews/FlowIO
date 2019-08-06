a260fe9062b108b5d99d2d107e082fa1a08425b6
whitews <whitews@gmail.com>
2019-08-06 09:45:02 -0400

    add license

--- setup.py
+++ setup.py
@@ -6,5 +6,8 @@ setup(
     packages=['flowio'],
     package_data={'': []},
     description='Flow Cytometry Standard I/O',
+    author="Scott White",
+    license='BSD',
+    url="https://github.com/whitews/flowio",
     requires=[],
 )
