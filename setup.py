66b0b380a8fdb8801d57e72cb4468e8a3e69de85
whitews <whitews@gmail.com>
2019-08-07 09:22:03 -0400

    bump version (again) to make sure license is in both source and dist

--- setup.py
+++ setup.py
@@ -2,12 +2,14 @@ from setuptools import setup
 
 setup(
     name='FlowIO',
-    version='0.9.4',
+    version='0.9.5',
     packages=['flowio'],
     package_data={'': []},
     description='Flow Cytometry Standard I/O',
     author="Scott White",
+    author_email="whitews@gmail.com",
     license='BSD',
     url="https://github.com/whitews/flowio",
     requires=[],
+    data_files=[("", ["LICENSE"])]
 )
