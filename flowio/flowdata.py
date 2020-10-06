8832483b0404d21ec2b93322c05b9b14542bcab4
whitews <whitews@gmail.com>
2020-10-06 16:06:28 -0400

    re-factor FlowData.write_fcs as a wrapper around create_fcs

--- flowio/flowdata.py
+++ flowio/flowdata.py
@@ -6,6 +6,7 @@ from warnings import warn
 import os
 import re
 from functools import reduce
+from .create_fcs import create_fcs
 
 try:
     # noinspection PyUnresolvedReferences, PyUnboundLocalVariable
@@ -393,99 +394,43 @@ class FlowData(object):
 
         return channels
 
-    def write_fcs(self, filename, extra=None):
-        def text_size(text_dict, text_delimiter):
-            result = text_delimiter
-            for idx in text_dict:
-                result += '$%s%s%s%s' % (
-                    idx,
-                    text_delimiter,
-                    text_dict[idx],
-                    text_delimiter)
-            return len(result), result
+    def write_fcs(self, filename, extra=None, extra_non_standard=None):
+        pnn_labels = []
+        pns_labels = []
 
-        # magic FCS defined positions
-        header_text_start = (10, 17)
-        header_text_end = (18, 25)
-        header_data_start = (26, 33)
-        header_data_end = (34, 41)
-        header_analysis_start = (42, 49)
-        header_analysis_end = (50, 5)
+        for k in sorted(self.channels.keys()):
+            pnn_labels.append(self.channels[k]['PnN'])
+
+            if 'PnS' in self.channels[k]:
+                pns_labels.append(self.channels[k]['PnS'])
+            else:
+                pns_labels.append('')
+
+        if 'spill' in self.text:
+            spill = self.text['spill']
+        else:
+            spill = None
+
+        if 'date' in self.text:
+            acq_date = self.text['date']
+        else:
+            acq_date = None
+
+        if 'cyt' in self.text:
+            cytometer = self.text['cyt']
+        else:
+            cytometer = None
 
         fh = open(filename, 'wb')
-        fh.write('FCS3.1')
-        fh.write(' ' * 53)
-
-        # Write TEXT Segment
-        text_start = 256  # arbitrarily start at byte 256.
-        delimiter = '/'  # use / as our delimiter.
-
-        # Write spaces until the start of the txt segment
-        fh.seek(58)
-        fh.write(' ' * (text_start - fh.tell()))
-
-        n_channels = int(self.text['par'])
-        n_points = len(self.events)
-        data_size = 4 * n_channels * n_points  # 4 bytes to hold float
-
-        text = dict()
-        text['BEGINANALYSIS'] = '0'
-        text['BEGINDATA'] = '0'
-        text['BEGINSTEXT'] = '0'
-        text['BYTEORD'] = '1,2,3,4'  # little endian
-        text['DATATYPE'] = 'F'  # only do float data
-        text['ENDANALYSIS'] = '0'
-        text['ENDDATA'] = '0'
-        text['ENDSTEXT'] = '0'
-        text['MODE'] = 'L'  # only do list mode data
-        text['NEXTDATA'] = '0'
-        text['PAR'] = str(n_channels)
-        text['TOT'] = str(n_points)
-        for i in range(n_channels):
-            text['P%dB' % (i + 1)] = '32'  # float requires 32 bits
-            text['P%dE' % (i + 1)] = '0,0'
-            text['P%dR' % (i + 1)] = str(max(self.events))
-            text['P%dN' % (i + 1)] = str(i)
-
-        if extra is not None:
-            for i in extra:
-                tmp = i.strip()
-                if tmp.lower() not in text and tmp.upper() not in text:
-                    val = extra[i].replace(delimiter, delimiter + delimiter)
-                    text[i] = val
-
-        i = 1
-        size, _ = text_size(text, delimiter)
-        prop_size = text_start + ((size % 256) + i) * 256
-        text['BEGINDATA'] = prop_size
-        text['ENDDATA'] = prop_size + data_size
-        data_start = prop_size
-        data_end = prop_size + data_size - 1
-        size, text_segment = text_size(text, delimiter)
-        text_end = text_start + size - 1
-
-        fh.write(text_segment)
-        fh.write(' ' * (data_start - fh.tell()))
-        fh.write(str(float(i) for i in self.events))
-
-        fh.seek(header_text_start[0])
-        fh.write(str(text_start))
-        fh.seek(header_text_end[0])
-        fh.write(str(text_end))
-
-        fh.seek(header_data_start[0])
-        if len(str(data_end)) < (header_data_end[1] - header_data_end[0]):
-            fh.write(str(data_start))
-            fh.seek(header_data_end[0])
-            fh.write(str(data_end))
-        else:
-            fh.write(str(0))
-            fh.seek(header_data_end[0])
-            fh.write(str(0))
-
-        fh.seek(header_analysis_start[0])
-        fh.write(str(0))
-        fh.seek(header_analysis_end[0])
-        fh.write(str(0))
-
+        fh = create_fcs(
+            self.events,
+            pnn_labels,
+            fh,
+            spill=spill,
+            cyt=cytometer,
+            date=acq_date,
+            extra=extra,
+            extra_non_standard=extra_non_standard,
+            opt_channel_names=pns_labels
+        )
         fh.close()
