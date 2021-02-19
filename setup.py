c6dc83f537868a20f3f2e3284f036499aab688ad
whitews <whitews@gmail.com>
2021-02-18 20:52:56 -0500

    bump to next beta patch version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='FlowIO',
-    version='0.9.8',
+    version='0.9.9b',
     packages=['flowio'],
     package_data={'': []},
     description='Flow Cytometry Standard I/O',
