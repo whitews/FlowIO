2690689675ac35533f0d9a1842d43f11b6df37b7
whitews <whitews@gmail.com>
2022-03-03 18:41:53 -0500

    add more thorough test cases for creating FCS files (there be bugs!)

--- flowio/tests/fcs_write_tests.py
+++ flowio/tests/fcs_write_tests.py
@@ -1,5 +1,6 @@
 import unittest
 import os
+import numpy as np
 from flowio import FlowData, create_fcs
 
 
@@ -21,3 +22,150 @@ class CreateFCSTestCase(unittest.TestCase):
         os.unlink(export_file_path)
 
         self.assertIsInstance(exported_flow_data, FlowData)
+
+    def test_create_fcs_data_offsets(self):
+        """
+        This tests whether FlowIO properly calculates
+        byte offset locations for the FCS data section
+
+        Create several small arrays of event data w/ only 1 channel.
+        The array lengths are chosen to produce a file where the data section
+        will end right before and after the 1000 byte offset location.
+        These will be files 01 & 02
+
+        Files 03 & 04 will have an extra metadata value for the keyword
+        $COM (used for storing a comment) that will push the text section
+        to a length that will make the data start byte location to be
+        just below and above 1000.
+
+        File 01:
+          4-bytes per float value * 138 event values = 552 bytes
+          header = 256 bytes
+          text section = 192 bytes
+          data start should be: 256 + 192 = 448
+          data end should be: 448 + 552 - 1 = 999
+
+        File 02:
+          4-bytes per float value * 139 event values = 556 bytes
+          header = 256 bytes
+          text section (by default here) = 193 bytes
+          data start should be: 256 + 193 = 449
+          data end should be: 449 + 556 - 1 = 1004
+
+        File 03:
+          4-bytes per float value * 138 event values = 552 bytes
+          header = 256 bytes
+          extra text: 551
+            4   bytes for '$COM'
+            544 bytes ($COM value size)
+            2   bytes (extra delimiters)
+            1   byte (extra digit for data end location)
+          text section  192 bytes (like File 01) + 551 (from above) = 743 bytes
+          data start should be: 256 + 743 = 999
+          data end should be: 999 + 552 - 1 = 1550
+
+        File 04:
+          4-bytes per float value * 138 event values = 552 bytes
+          header = 256 bytes
+          extra text: 553
+            4   bytes for '$COM'
+            545 bytes ($COM value size)
+            2   bytes (extra delimiters)
+            2   byte (extra digit for both data begin & data end location)
+          text section  192 bytes (like File 01) + 553 (from above) = 745 bytes
+          data start should be: 256 + 745 = 1001
+          data end should be: 1001 + 552 - 1 = 1552
+        """
+
+        n_events_list = [138.0, 139.0]
+        event_data_01 = list(np.arange(n_events_list[0]))
+        event_data_02 = list(np.arange(n_events_list[1]))
+
+        comment_value_01 = 'x' * 544
+        comment_value_02 = 'x' * 545
+
+        pnn_labels = ['FSC-A']
+
+        export_file_path = "examples/fcs_files/test_fcs_export_data_offsets.fcs"
+
+        # File 01
+        fh = open(export_file_path, 'wb')
+        create_fcs(event_data_01, channel_names=pnn_labels, file_handle=fh)
+        fh.close()
+
+        exported_flow_data_01 = FlowData(export_file_path)
+        os.unlink(export_file_path)
+
+        # File 02
+        fh = open(export_file_path, 'wb')
+        create_fcs(event_data_02, channel_names=pnn_labels, file_handle=fh)
+        fh.close()
+
+        exported_flow_data_02 = FlowData(export_file_path)
+        os.unlink(export_file_path)
+
+        # File 03
+        fh = open(export_file_path, 'wb')
+        extra_dict = {'COM': comment_value_01}
+        create_fcs(event_data_01, channel_names=pnn_labels, file_handle=fh, extra=extra_dict)
+        fh.close()
+
+        exported_flow_data_03 = FlowData(export_file_path)
+        os.unlink(export_file_path)
+
+        # File 04
+        fh = open(export_file_path, 'wb')
+        extra_dict = {'COM': comment_value_02}
+        create_fcs(event_data_01, channel_names=pnn_labels, file_handle=fh, extra=extra_dict)
+        fh.close()
+
+        exported_flow_data_04 = FlowData(export_file_path)
+        os.unlink(export_file_path)
+
+        self.assertIsInstance(exported_flow_data_01, FlowData)
+        self.assertListEqual(event_data_01, list(exported_flow_data_01.events))
+        self.assertEqual(exported_flow_data_01.header['data_start'], 448)
+        self.assertEqual(exported_flow_data_01.header['data_end'], 999)
+
+        self.assertIsInstance(exported_flow_data_02, FlowData)
+        self.assertListEqual(event_data_02, list(exported_flow_data_02.events))
+        self.assertEqual(exported_flow_data_02.header['data_start'], 449)
+        self.assertEqual(exported_flow_data_02.header['data_end'], 1004)
+
+        self.assertIsInstance(exported_flow_data_03, FlowData)
+        self.assertListEqual(event_data_01, list(exported_flow_data_03.events))
+        self.assertEqual(exported_flow_data_03.header['data_start'], 999)
+        self.assertEqual(exported_flow_data_03.header['data_end'], 1550)
+
+        self.assertIsInstance(exported_flow_data_04, FlowData)
+        self.assertListEqual(event_data_01, list(exported_flow_data_04.events))
+        self.assertEqual(exported_flow_data_04.header['data_start'], 1001)
+        self.assertEqual(exported_flow_data_04.header['data_end'], 1552)
+
+    def test_create_large_fcs(self):
+        # create 100,000,000 bytes of event data
+        # 4-bytes per float value * 4 channels * 6,250,000 events
+        n_events = 6250000
+        n_channels = 4
+        np.random.seed(1)
+        event_data = np.random.random(n_channels * n_events).tolist()
+
+        pnn_labels = [
+            'FSC-A',
+            'SSC-A',
+            'FLR1-A',
+            'FLR2-A'
+        ]
+
+        export_file_path = "examples/fcs_files/test_large_fcs_export.fcs"
+        fh = open(export_file_path, 'wb')
+        create_fcs(event_data, channel_names=pnn_labels, file_handle=fh)
+        fh.close()
+
+        exported_flow_data = FlowData(export_file_path)
+        os.unlink(export_file_path)
+
+        self.assertIsInstance(exported_flow_data, FlowData)
+        self.assertEqual(exported_flow_data.header['data_start'], 0)
+        self.assertEqual(exported_flow_data.header['data_end'], 0)
+        self.assertGreater(int(exported_flow_data.text['enddata']), 99999999)
