4246faff907dfa6a57e87f383a00012db0930f7f
whitews <whitews@gmail.com>
2022-03-23 13:03:24 -0400

    more tests for channel parameter metadata in create_fcs function

--- flowio/tests/fcs_write_tests.py
+++ flowio/tests/fcs_write_tests.py
@@ -274,6 +274,60 @@ class CreateFCSTestCase(unittest.TestCase):
         export_file_path = "examples/fcs_files/test_fcs_export.fcs"
         fh = open(export_file_path, 'wb')
 
+        create_fcs(fh, event_data, channel_names=pnn_labels, metadata_dict=metadata_dict)
+
+        fh.close()
+
+        exported_flow_data = FlowData(export_file_path)
+        os.unlink(export_file_path)
+
+        self.assertEqual(exported_flow_data.text['p9e'], '4,1')
+
+    def test_create_fcs_with_log0_pne(self):
+        """
+        This tests using the invalid PnE log0 value of 0.
+        Unfortunately, this is commonly found in files, and
+        FCS 3.1 states to treat it as a 1. create_fcs will
+        do this automatically if given decades > 0 and
+        log0 == 0
+        """
+        event_data = self.flow_data.events
+        channel_names = self.flow_data.channels
+        pnn_labels = [v['PnN'] for k, v in channel_names.items()]
+
+        metadata_dict = {
+            'p9e': '4,0'
+        }
+
+        export_file_path = "examples/fcs_files/test_fcs_export.fcs"
+        fh = open(export_file_path, 'wb')
+
+        create_fcs(fh, event_data, channel_names=pnn_labels, metadata_dict=metadata_dict)
+
+        fh.close()
+
+        exported_flow_data = FlowData(export_file_path)
+        os.unlink(export_file_path)
+
+        self.assertEqual(exported_flow_data.text['p9e'], '4,1')
+
+    def test_create_fcs_with_invalid_gain_with_log_pne(self):
+        """
+        This tests using the invalid combination of a log
+        scaling PnE value & gain value != 1.
+        """
+        event_data = self.flow_data.events
+        channel_names = self.flow_data.channels
+        pnn_labels = [v['PnN'] for k, v in channel_names.items()]
+
+        metadata_dict = {
+            'p9e': '4,1',
+            'p9g': '2'
+        }
+
+        export_file_path = "examples/fcs_files/test_fcs_export.fcs"
+        fh = open(export_file_path, 'wb')
+
         self.assertRaises(
             ValueError,
             create_fcs,
@@ -285,6 +339,34 @@ class CreateFCSTestCase(unittest.TestCase):
 
         fh.close()
         os.unlink(export_file_path)
+
+    def test_create_fcs_with_pnr(self):
+        """
+        This tests using the invalid PnE log0 value of 0.
+        Unfortunately, this is commonly found in files, and
+        FCS 3.1 states to treat it as a 1. create_fcs will
+        do this automatically if given decades > 0 and
+        log0 == 0
+        """
+        event_data = self.flow_data.events
+        channel_names = self.flow_data.channels
+        pnn_labels = [v['PnN'] for k, v in channel_names.items()]
+
+        metadata_dict = {
+            'p9r': '2048'
+        }
+
+        export_file_path = "examples/fcs_files/test_fcs_export.fcs"
+        fh = open(export_file_path, 'wb')
+
+        create_fcs(fh, event_data, channel_names=pnn_labels, metadata_dict=metadata_dict)
+
+        fh.close()
+
+        exported_flow_data = FlowData(export_file_path)
+        os.unlink(export_file_path)
+
+        self.assertEqual(exported_flow_data.text['p9r'], '2048')
 
     def test_create_fcs_ignore_extra_pnn(self):
         event_data = self.flow_data.events
