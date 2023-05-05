ebe8b78109fac8da8c0177ef4e474bf7b57db4ec
whitews <whitews@gmail.com>
2023-05-05 16:59:41 -0400

    explicit imports in init

--- flowio/__init__.py
+++ flowio/__init__.py
@@ -1,4 +1,13 @@
 from .flowdata import FlowData
 from .create_fcs import create_fcs
 from .utils import read_multiple_data_sets
+from . import exceptions as exceptions  # noqa
+
 from ._version import __version__
+
+__all__ = [
+    'FlowData',
+    'create_fcs',
+    'read_multiple_data_sets',
+    'exceptions'
+]
