50caf6ad6877312b6c9e698c0458665b45281bd0
Maxmilian Bertsch <max.bertsch@gmx.de>
2023-05-15 15:13:01 +0200

    add test for fcs3.0 data of lmd file

--- flowio/tests/flowdata_lmd_tests.py
+++ flowio/tests/flowdata_lmd_tests.py
@@ -1,6 +1,7 @@
 import unittest
 import warnings
-from flowio import FlowData
+import array
+from flowio import FlowData, read_multiple_data_sets
 from flowio.exceptions import FCSParsingError
 
 
@@ -8,10 +9,9 @@ class FlowDataLMDTestCase(unittest.TestCase):
     def setUp(self):
         with warnings.catch_warnings():
             warnings.simplefilter('ignore')
-            self.flow_data = FlowData(
+            self.flow_data, self.fcs3_data = read_multiple_data_sets(
                 'examples/fcs_files/coulter.lmd',
-                ignore_offset_error=True,
-                nextdata_offset=0
+                ignore_offset_error=True
             )
         
     def test_event_count(self):
@@ -26,3 +26,11 @@ class FlowDataLMDTestCase(unittest.TestCase):
     def test_fail_data_offset_error(self):
         with self.assertRaises(FCSParsingError):     
             FlowData('examples/fcs_files/coulter.lmd', nextdata_offset=0)
+
+    def test_right_integer_reading(self):
+        self.assertEqual(
+            self.fcs3_data.events[:24], 
+            array.array("I", [61056, 131840, 46, 324, 10309, 104, 11912, 0,
+                            257280, 378656, 139, 1728, 58688, 354, 58720, 0,
+                            164128, 305376, 159, 924, 29024, 208, 29728, 0])
+        )
