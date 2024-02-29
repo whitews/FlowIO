d8f8ed39d5bd63cd1700397044a6b5b20de3a8b1
whitews <whitews@gmail.com>
2024-02-29 15:12:56 -0500

    move most project metadata to pyproject.toml

--- setup.py
+++ setup.py
@@ -12,30 +12,8 @@ exec(open(VERSION_FILE).read())
 if not __version__:
     raise RuntimeError("__version__ string not found in file %s" % VERSION_FILE)
 
-with open('README.md', 'r') as fh:
-    long_description = fh.read()
-
 setup(
-    name='FlowIO',
-    version=__version__,
     packages=['flowio'],
     package_dir={'': "src"},
-    package_data={'': []},
-    description='FlowIO is a Python library for reading / writing Flow Cytometry Standard (FCS) files',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    author='Scott White',
-    author_email='whitews@gmail.com',
-    license='BSD',
-    license_files=('LICENSE',),
-    url='https://github.com/whitews/flowio',
-    requires=[],
-    classifiers=[
-        'Programming Language :: Python :: 3.12',
-        'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.7'
-    ]
+    package_data={'': []}
 )
