3c58052131ad3a63ef3d136ee0d112bb526f78a9
whitews <whitews@gmail.com>
2025-03-27 10:57:00 -0400

    update example scripts

--- examples/compensate_example.py
+++ examples/compensate_example.py
@@ -1,15 +1,14 @@
 import flowio
 import flowutils
-import numpy
 
 fd = flowio.FlowData('fcs_files/100715.fcs')
 spill, markers = flowutils.compensate.get_spill(fd.text['spill'])
-events = numpy.reshape(fd.events, (-1, fd.channel_count))
+events = fd.as_array()
 
 fluoro_indices = []
 
 for channel in fd.channels:
-    if fd.channels[channel]['PnN'] in markers:
+    if fd.channels[channel]['pnn'] in markers:
         fluoro_indices.append(int(channel) - 1)
 
 fluoro_indices.sort()
--- examples/reshape_events.py
+++
@@ -1,7 +0,0 @@
-import flowio
-import numpy
-
-f = flowio.FlowData('fcs_files/data1.fcs')
-n = numpy.reshape(f.events, (-1, f.channel_count))
-
-print(n.shape)
