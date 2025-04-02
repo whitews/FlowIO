436df169fd9cbffeb8884abdf469e257d46ad556
whitews <whitews@gmail.com>
2025-04-02 10:00:24 -0400

    fix RTD docs - attempt #9

--- docs/conf.py
+++ docs/conf.py
@@ -10,7 +10,7 @@ import sys
 on_rtd = os.environ.get('READTHEDOCS') == 'True'
 if on_rtd:
     print(os.getcwd())
-    sys.path.insert(0, os.path.abspath('../..'))
+    sys.path.insert(0, os.path.abspath('..'))
 else:
     sys.path.insert(0, os.path.abspath('../src'))
 
